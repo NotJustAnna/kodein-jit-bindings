e46163858d64f25f001ec4249536ca3233c6287d
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2018-09-19 19:32:50 -0300

    updated to 5.2.0

---
+++ src/main/kotlin/net/notjustanna/kodein/jit/annotations.kt
@@ -0,0 +1,15 @@
+package net.notjustanna.kodein.jit
+
+/**
+ * Defines that this should be the constructor used to build.
+ */
+@Target(AnnotationTarget.CONSTRUCTOR)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class JIT
+
+/**
+ * Defines that this class instance should be unique per container.
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Singleton
